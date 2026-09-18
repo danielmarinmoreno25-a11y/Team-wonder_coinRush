62914fcecc881964a5d41c5afd0e207199ba86d0
danielmarinmoreno25-a11y <daniel.marinmoreno25@campusfp.es>
2026-09-18 12:46:54 +0200

    unity base

---
+++ WONDER_Coin Rash/Coinrush/Assets/scripts/time botton.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timebotton : MonoBehaviour
+{
+    private float time;
+    time = 60;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ()
+        {
+
+        }
+
+        else
+        {
+
+        }
+    }
+}
---
+++ WONDER_Coin Rash/Coinrush/Assets/scripts/winlost.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class winlost : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject menudeperderGO; // creamos una variable tipo gameobjet porque es el tipo de dato que guarda todo nuestro menu
+    public bool a;
+    // Start is called before the first frame update
+    void Start()
+    {
+        a = true;// lo ponemos a true
+        Time.timeScale = 1.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Has chocado con " + collision.gameObject.name);
+    }
+
+    public void ReintentarPerder()
+    {
+        SceneManager.LoadScene(1);// cargamos la escena 1, que es el juego y la cargamos en modo simple para no cargarla una encima de otra
+        Time.timeScale = 1.0f; // activamos el tiempo
+    }
+
+    public void ExitPerder()
+    {
+        a = true; // reanudamos
+        Time.timeScale = 0.0f; // activamos el tiempo
+        SceneManager.LoadScene(0);
+
+    }
+
+    public void ReintentarGanar()
+    {
+        SceneManager.LoadScene(1);// cargamos la escena 1, que es el juego y la cargamos en modo simple para no cargarla una encima de otra
+        Time.timeScale = 1.0f; // activamos el tiempo
+    }
+
+    public void ExitGanar()
+    {
+        a = true; // reanudamos
+        Time.timeScale = 0.0f; // activamos el tiempo
+        SceneManager.LoadScene(0);
+    }
+}
