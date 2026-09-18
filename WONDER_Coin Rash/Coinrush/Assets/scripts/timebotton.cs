04d46d01b8f9ea0daa2e5b39f0b55979fe1be465
Lukas <itziar.ugenaisabel.23@campusfp.es>
2026-09-18 14:14:21 +0200

    modificación 1 coin rash

--- WONDER_Coin Rash/Coinrush/Assets/scripts/timebotton.cs
+++ WONDER_Coin Rash/Coinrush/Assets/scripts/timebotton.cs
@@ -4,25 +4,43 @@ using UnityEngine;
 
 public class timebotton : MonoBehaviour
 {
-    private float time;
+    public float time;
+    public GameObject CanvastimeGO;
+    private bool pausedtime;
     // Start is called before the first frame update
     void Start()
     {
-        time = 60;
-
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*if ()
-        {
+        time -= Time.deltaTime;
+        times();
+    }
 
+    public void times()
+    {
+        if (time <= 0)
+        {
+            pausedtime = true;
+            Time.timeScale = 0.0f;
+            CanvastimeGO.SetActive(true);
+        }
+
+        else if (time >= 60)
+        {
+            pausedtime = true;
+            Time.timeScale = 0.0f;
+            CanvastimeGO.SetActive(true);
         }
 
         else
         {
-
-        }*/
+            pausedtime = false;
+            Time.timeScale = 1.0f;
+            CanvastimeGO.SetActive(false);
+        }
     }
 }
