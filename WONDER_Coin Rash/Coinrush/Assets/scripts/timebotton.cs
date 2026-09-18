b909f669a76cb0033d747e442c316f316d17fba0
danielmarinmoreno25-a11y <daniel.marinmoreno25@campusfp.es>
2026-09-18 13:36:57 +0200

    unity update 1

--- WONDER_Coin Rash/Coinrush/Assets/scripts/timebotton.cs
+++ WONDER_Coin Rash/Coinrush/Assets/scripts/timebotton.cs
@@ -4,25 +4,17 @@ using UnityEngine;
 
 public class timebotton : MonoBehaviour
 {
-    private float time;
-    time = 60;
+    public float time;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        time = 60;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if ()
-        {
-
-        }
-
-        else
-        {
-
-        }
+        
     }
 }
